37fb59e697a4b5958177bcc0db50601efef904bc
ferdiyilmaz95 <44038174+ferdiyilmaz95@users.noreply.github.com>
2018-10-10 22:54:50 +0300

    ilk taahhüt

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BT-709 Algoritması";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getirToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // getirToolStripMenuItem
+            // 
+            this.getirToolStripMenuItem.Name = "getirToolStripMenuItem";
+            this.getirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.getirToolStripMenuItem.Text = "Getir";
+            this.getirToolStripMenuItem.Click += new System.EventHandler(this.getirToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(241, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 218);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(463, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(337, 218);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Luma Algoritması";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 24);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Açıklık Algoritması";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 25);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Kanal Çıkarımı Algoritması Kırmızı";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 25);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Kanal Çıkarımı Algoritması Yeşil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 220);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(169, 25);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Kanal Çıkarımı Algoritması Mavi";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getirToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap kaynak, islem;
+
+        private void getirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+            if (sonuc == DialogResult.OK)
+            {
+                kaynak = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = kaynak;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = Convert.ToInt32 (renkliRenk.R*0.2125 + renkliRenk.G*0.7154 + renkliRenk.B*0.072);
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = Convert.ToInt32(renkliRenk.R * 0.3 + renkliRenk.G * 0.59 + renkliRenk.B * 0.11);
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+
+                    byte[] renkDizi = { renkliRenk.R, renkliRenk.G, renkliRenk.B };
+                    Array.Sort(renkDizi);
+
+                    int gri = Convert.ToInt32((renkDizi[0] + renkDizi[2]) / 2);
+
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+
+                    int gri = renkliRenk.R;
+
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+
+                    int gri = renkliRenk.G;
+
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+
+                    int gri = renkliRenk.B;
+
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
